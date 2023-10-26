f5175de72489d1cc30e298bbe5d262297ec8efc6
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2023-10-26 17:55:35 +0100

    Create resolver.js

---
+++ resolver.js
@@ -0,0 +1,9 @@
+const Query = {
+    TwoThousandAndFourteen: () => '$17,824,071,733.82',
+    TwoThousandAndFifteen: () => '$18,150,617,666,484.33',
+    TwoThousandAndSixteen: () => '$19,573,444,713,936.7',
+    TwoThousandAndSeventeen: () => '$20,244,900,016,053.51',
+    TwoThousandAndEighteen: () => '$21,516,058,183,180.2',
+    TwoThousandAndNineteen: () => '$22,719,401,753,433.7'
+}
+module.exports = {Query}
